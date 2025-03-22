c95961528f9ea9b6619a5c4b8a6e9ab32e5a823d
CRBroughton <crbroughton@posteo.uk>
2025-03-22 11:46:47 +0000

    test: :white_check_mark: add passing tests for removal of colours by id, and the initial colour

--- src/store/colours.test.ts
+++ src/store/colours.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect, beforeEach } from 'vitest';
 import { configureStore } from '@reduxjs/toolkit';
-import coloursReducer from '../store/colours';
+import coloursReducer, { addColour, removeColour } from '../store/colours';
 import type {  Store } from '../store'; 
 
 describe('Colours Reducer', () => {
@@ -25,4 +25,26 @@ describe('Colours Reducer', () => {
       expect(state.syncSettings).toEqual({ l: false, c: false, h: false });
     });
   });
+
+  describe("removeColour tests", () => {
+    it("should remove a colour by its ID", () => {
+        store.dispatch(addColour())
+
+        const stateCountAfterColourAddition = store.getState().colours.colourTypes
+        expect(stateCountAfterColourAddition).toHaveLength(2)
+
+        store.dispatch(removeColour(2))
+
+        const stateAfterRemoval = store.getState().colours
+        expect(stateAfterRemoval.colourTypes).toHaveLength(1)
+        expect(stateAfterRemoval.colourTypes[0].id).toBe(1)
+    })
+  })
+
+  it("can handle the removal of the initial colour", () => {
+    store.dispatch(removeColour(1))
+
+    const state = store.getState().colours
+    expect(state.colourTypes).toHaveLength(0)
+  })
 });
